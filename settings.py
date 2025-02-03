345587f7869188fb91dd64c88e9efaad1355ab4e
@andrei_kotov1 <kotov.aa.logist@gmail.com>
2025-02-03 22:10:32 +0300

    Add multi sprite

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 0, 139)
+        self.game_caption = "Alien Invasion"
+
+        self.ship_speed = 1.5
---
+++ ship.py
@@ -0,0 +1,49 @@
+import pygame
+
+class Ship():
+    def __init__(self, ai_game):
+        # Инициализирует корабль и задает его начальную позицию
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
+
+        # Загружает изображение корабля и получает прямоугольник
+        self.images = pygame.image.load('images/ships5.png')
+        self.anim = []
+        self.fill_anim()
+        self.current_img = 0
+        self.rect = self.anim[self.current_img].get_rect()
+
+        # Каждый новый корабль появляется у края экрана
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        self.x = float(self.rect.x)
+        self.moving_right = False
+        self.moving_left = False
+
+
+    def fill_anim(self):
+        for i in range(0, 4):
+            sub_s = self.images.subsurface(pygame.Rect(0, 0, 152, 207))
+            sub_s = pygame.transform.smoothscale(sub_s, (100, 130))
+            self.anim.append(sub_s)
+
+    def update_image(self):
+        if self.current_img > 4:
+            self.current_img = 0
+        self.current_img += 1
+
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
+        self.image = self.anim[self.current_img]
+
+    def blitime(self):
+        # Рисует корабль в текущей позиции
+        self.screen.blit(self.anim[self.current_img], self.rect)
+
+
